0c7aa4d2054b603a78b228816cd9681fdf0522ed
adityayadav2656 <adityayadav25252@gmail.com>
2025-05-22 08:56:51 +0530

    set a map in moblie

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+
+ import React from 'react';
 
 const MapComponent = () => {
   return (
-    <div className="w-full h-screen flex justify-center flex-col items-center">
-      <h1 className='w-[98%] h-1 bg-blue-800 absolute top-[70px]  '></h1>
+    <div className="relative w-screen h-full overflow-x-auto overflow-y-auto">
+      {/* Optional: horizontal line */}
+      <h1 className='w-[98%] h-1 bg-blue-800 absolute top-[40px]'></h1>
+
       <iframe 
-      
-        src="/map/index.html"  // Directly refer to the path relative to the public folder
-        width="100%"  // Adjust the width to a percentage
-        height="100%" // Adjust the height to a percentage
+        src="/map/index.html"  
         title="College Map"
-        style={{ border: "none", boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)", paddingTop:"60px" }}
+        className="min-w-[1450px] min-h-full"
+        style={{
+          border: "none",
+          boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+          paddingTop: "60px"
+        }}
       ></iframe>
     </div>
   );
